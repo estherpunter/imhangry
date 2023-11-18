c3194023ce2ba4c5fd0adb626f15217eaa1784a6
estherpunter <estherpunter5@hotmail.com>
2023-11-18 22:38:47 +0100

    Allrecipes request gemaakt

--- src/pages/allrecipes/AllRecipes.jsx
+++ src/pages/allrecipes/AllRecipes.jsx
@@ -1,22 +1,35 @@
-import Recipe from "../../components/recipe/Recipe.jsx";
-import {useState} from "react";
+import {useEffect, useState} from "react";
+import axios from "axios";
 
 function AllRecipes() {
     const [error, toggleError] = useState(false);
     const [loading, toggleLoading] = useState(false);
 
 
+    useEffect(() => {
+        console.log('ik ben gemount')
 
+        async function fetchData() {
+            toggleLoading(true);
+
+            try {
+                const response = await axios.get('https://api.edamam.com/api/recipes/v2?type=public&app_id=48b29177&app_key=1f0f159ce52371ca9f5937111c6446c1&diet=balanced');
+                console.log(response.data.hits);
+            } catch (e) {
+                console.error(e);
+                toggleError(true)
+            }
+        }
+
+        void fetchData;
+
+    }, []);
 
 
     return (
         <>
             <h1>All recipes</h1>
             <p>Here you can browse through all the recipes!</p>
-
-            <button type="button" >Search</button>
-
-
 
             {error && <p className="error">Something went wrong with fetching the data</p>}
             {loading && <p className="loading">Loading...</p>}

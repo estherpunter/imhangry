902584614a60ca454ac738ab031cd7a73022ec63
estherpunter <estherpunter5@hotmail.com>
2024-02-02 14:44:57 +0100

    env file proberen aan te maken

--- src/pages/allrecipes/AllRecipes.jsx
+++ src/pages/allrecipes/AllRecipes.jsx
@@ -2,14 +2,15 @@ import {useEffect, useState} from "react";
 import axios from "axios";
 import Recipe from "../../components/recipe/Recipe.jsx";
 import './AllRecipes.css';
+import process from "../../../.eslintrc.cjs";
 
 function AllRecipes() {
     const [error, toggleError] = useState(false);
     const [loading, toggleLoading] = useState(false);
     const [recipes, setRecipes] = useState([])
 
-    const appId = '48b29177';
-    const appKey = '1f0f159ce52371ca9f5937111c6446c1'
+    const appId = `${process.env.REACT_APP_API_ID}`;
+    const appKey = `${process.env.REACT_APP_API_KEY}`
 
 
     useEffect(() => {
